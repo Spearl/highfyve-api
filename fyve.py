83f56cebc56f14b1db9254512ad713f5f0480374
Landon Spear <landon@path.com>
2014-09-27 11:22:11 -0700

    assign redis to app

--- fyve.py
+++ fyve.py
@@ -14,7 +14,7 @@ TWITTER_API_KEY = os.getenv('TWITTER_API_KEY')
 TWITTER_API_SECRET = os.getenv('TWITTER_API_SECRET')
 app = make_json_app(__name__)
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-redis = redis.from_url(redis_url)
+app.redis = redis.from_url(redis_url)
 
 
 @app.route('/')
