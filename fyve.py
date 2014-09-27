3102d0e9829f6b2aa065c1dbf88fca0d2993da18
Landon Spear <landon@path.com>
2014-09-27 10:19:15 -0700

    Add logging and error handling

--- fyve.py
+++ fyve.py
@@ -1,8 +1,13 @@
+import logging
 import os
-from flask import Flask
 import redis
 
-app = Flask(__name__)
+from make_app import make_json_app
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+app = make_json_app(__name__)
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 redis = redis.from_url(redis_url)
 
