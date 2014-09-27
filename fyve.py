1892b41bcc60e77aa3d0c143340bd80bb87e343b
Landon Spear <landon@path.com>
2014-09-27 10:01:38 -0700

    Add redis

--- fyve.py
+++ fyve.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask
+import redis
 
 app = Flask(__name__)
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis = redis.from_url(redis_url)
 
 @app.route('/')
 def hello():
